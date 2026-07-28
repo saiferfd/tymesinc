5f79d9668bd23bdfc46a68bd3390e551bf5f55e4
saiferfd <saiferfand@gmail.com>
2026-07-28 16:18:16 +0300

    Update timesync.js

--- timesync.js
+++ timesync.js
@@ -7,11 +7,9 @@
     var timeStalledCount = 0; 
     var nextEpisodeTriggered = false;
     var currentSelector = '';
-    var playbackStarted = false;
 
-    // 1. Ловимо клік по серії (Онлайн або Торрент)
+    // Ловимо клік по серії
     $(document).on('click hover:enter', '.torrent-list > .online-prestige.selector, .torrent-files > .torrent-serial.selector, .torrent-files > .torrent-item.selector', function () {
-        
         if ($(this).hasClass('online-prestige')) currentSelector = '.torrent-list > .online-prestige.selector';
         else if ($(this).hasClass('torrent-serial')) currentSelector = '.torrent-files > .torrent-serial.selector';
         else currentSelector = '.torrent-files > .torrent-item.selector';
@@ -21,15 +19,12 @@
         
         nextEpisodeTriggered = false;
         timeStalledCount = 0;
-        playbackStarted = false;
 
-        // Даємо Лампі чверть секунди на створення запису в базі і беремо актуальний останній хеш
         setTimeout(function() {
             try {
                 var fileViews = Lampa.Storage.get('file_view', {});
                 var keys = Object.keys(fileViews);
                 if (keys.length > 0) {
-                    // Беремо найсвіжіший запис із бази Лампи
                     currentHash = keys[keys.length - 1];
                     var info = fileViews[currentHash];
                     lastSeenTime = (info && info.time) ? info.time : 0;
@@ -48,18 +43,22 @@
             var $next = $allEpisodes.eq(nextIndex);
             currentEpisodeIndex = nextIndex;
             timeStalledCount = 0;
-            playbackStarted = false;
             
+            // Імітуємо повноцінний клік пультом/мишкою, щоб збірка сприйняла це як ручний запуск
+            $next.trigger('hover:enter');
+            $next.trigger('click');
             var el = $next[0];
-            if (el) el.dispatchEvent(new MouseEvent('click', { view: window, bubbles: true, cancelable: true }));
+            if (el) {
+                el.dispatchEvent(new MouseEvent('click', { view: window, bubbles: true, cancelable: true }));
+            }
 
-            setTimeout(function() { nextEpisodeTriggered = false; }, 5000);
+            setTimeout(function() { nextEpisodeTriggered = false; }, 6000);
         } else {
             Lampa.Noty.show('Автоперехід: Це остання серія');
         }
     }
 
-    // 2. Фоновий сканер (стежить виключно за закінченням серії)
+    // Фоновий сканер
     setInterval(function() {
         if (!currentHash || nextEpisodeTriggered || currentEpisodeIndex === -1) return;
 
@@ -71,16 +70,6 @@
             var percentWatched = currentTime / info.duration;
             var timeLeft = info.duration - currentTime;
 
-            // Захист від хибного спрацьовування на старті
-            if (!playbackStarted) {
-                if (Math.abs(currentTime - lastSeenTime) >= 1) {
-                    playbackStarted = true;
-                    lastSeenTime = currentTime;
-                }
-                return; 
-            }
-
-            // Перевіряємо, чи час зупинився (VLC закрився)
             if (Math.abs(currentTime - lastSeenTime) < 2) { 
                 timeStalledCount++; 
             } else {
@@ -88,14 +77,16 @@
                 lastSeenTime = currentTime;
             }
 
-            // Умова завершення серії
+            // Якщо серія закінчилась
             if (timeStalledCount >= 2 && (percentWatched > 0.85 || timeLeft <= 180)) {
                 nextEpisodeTriggered = true;
-                Lampa.Noty.show('Серія завершена. Запуск наступної...');
-                playNextEpisode();
+                Lampa.Noty.show('Серія завершена. Перемикаємо...');
+                
+                // Даємо невелику паузу, щоб Лампа остаточно закрила попередній процес
+                setTimeout(playNextEpisode, 1500);
             }
         }
     }, 2000);
 
-    console.log('Lampa Auto-Next: Режим мʼякого читання бази активовано');
+    console.log('Lampa Auto-Next: Режим імітації нативного кліку активовано');
 })();
